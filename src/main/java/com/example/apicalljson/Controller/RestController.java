0bbb569b9a34b3161cb0fd738e37d0f3a93a84f7
durga <dmdu23@gmail.com>
2023-10-10 00:45:20 +0530

    FileSetup

---
+++ src/main/java/com/example/apicalljson/Controller/RestController.java
@@ -0,0 +1,4 @@
+package com.example.apicalljson.Controller;
+
+public class RestController {
+}
---
+++ src/test/java/com/example/apicalljson/ApiCallJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicalljson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCallJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
